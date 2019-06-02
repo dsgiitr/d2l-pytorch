6310bc9058aa7090cefcf3dacbc6911997a3cb71
Anirudh Dagar <anirudhdagar6@gmail.com>
2019-06-02 10:41:02 +0530

    add train_ch5 & gpu ready eval

--- d2l/train.py
+++ d2l/train.py
@@ -6,16 +6,18 @@ from .figure import set_figsize, plt
 import torch
 import torch.optim as optim
 
-__all__ = ['evaluate_accuracy', 'train_ch3']
+__all__ = ['evaluate_accuracy', 'train_ch3', 'train_ch5']
 
-def evaluate_accuracy(data_iter, net):
+def evaluate_accuracy(data_iter, net, device=torch.device('cpu')):
     """Evaluate accuracy of a model on the given data set."""
-    acc_sum, n = torch.Tensor([0]), 0
+    acc_sum, n = torch.Tensor([0]).to(device), 0
     for batch in data_iter:
         features, labels = batch
         for X, y in zip(features, labels):
-            y = y.type(torch.FloatTensor)
-            acc_sum += torch.sum((torch.argmax(net(X), dim=1).type(torch.FloatTensor) == y).detach()).float()
+            # If device is the GPU, copy the data to the GPU.
+            y = y.type(torch.float32)
+            X, y = X.to(device), y.to(device)
+            acc_sum += torch.sum((torch.argmax(net(X), dim=1).type(torch.float32) == y).detach()).float()
             n += 1
     return acc_sum.item() / n
 
@@ -40,3 +42,27 @@ def train_ch3(net, train_iter, test_iter, criterion, num_epochs, batch_size, lr=
         test_acc = evaluate_accuracy(test_iter, net)
         print('epoch %d, loss %.4f, train acc %.3f, test acc %.3f'
               % (epoch + 1, train_l_sum / n, train_acc_sum / n, test_acc))
+
+
+def train_ch5(net, train_iter, test_iter, criterion, num_epochs, batch_size, device, lr=None):
+    print('training on', device)
+    optimizer = optim.SGD(net.parameters(), lr=lr)
+    for epoch in range(num_epochs):
+        train_l_sum, train_acc_sum, n, start = 0.0, 0.0, 0, time.time()
+        for X, y in train_iter:
+            X, y = X.to(device), y.to(device)
+            optimizer.zero_grad()
+
+            y_hat = net(X)
+            loss = criterion(y_hat, y)
+            loss.backward()
+            optimizer.step()
+
+            y = y.type(torch.float32)
+            y.to(device)
+            train_l_sum += loss.item()
+            train_acc_sum += torch.sum((torch.argmax(y_hat, dim=1).type(torch.float32) == y).detach()).float()
+            n += list(y.size())[0]
+        test_acc = evaluate_accuracy(test_iter, net, device)
+        print('epoch %d, loss %.4f, train acc %.3f, test acc %.3f, time %.1f sec'
+              % (epoch + 1, train_l_sum / n, train_acc_sum / n, test_acc, time.time() - start))

bcf8a900a9888d1e39002f18201455916aae5353
Saswat Das <saswat.papp43@gmail.com>
2019-06-04 12:27:01 +0530

    Lenet mods + NiN(new addition) + train.py improvement (#35)
    * Lenet mods + NiN(new addition) + train.py improvement
    
    1.Lenet is tested on the new train_ch5() function
    2.train_ch5() has following chanages
    	a. Loss and accuracy calculated on torch tensors on the device so it improves speed
    	b. y is not typecasted to float for comparision. Instead as argmax() returns a long tensor, y is compared as it is
    	c. Accuracy calculation is done inside torch.no_grad() block . No need for detach()
    3. Evaluate Accuracy has the following changes
    	a. X and y are directly collected from test_iter rather than using second for loop and zipping features and labels( like in old one) It utilizes vectorized implementation rather than usage of loops
    	b. acc_sum is now a tensor in the specified device(improved speed)
    	c. torch.no_grad() removes the requirement of detach()
    4. Netowrk in Network Model was added (tested in Google Colab GPU with better accuracy than book even with same paramters(lr,batch,init)
    
    * add arg spaces d2l/train.py
    
    Co-Authored-By: Anirudh <anirudhdagar6@gmail.com>
    
    * Update d2l/train.py
    
    Co-Authored-By: Anirudh <anirudhdagar6@gmail.com>
    
    * Update d2l/train.py
    
    Co-Authored-By: Anirudh <anirudhdagar6@gmail.com>
    
    * Update d2l/train.py
    
    Co-Authored-By: Anirudh <anirudhdagar6@gmail.com>
    
    * Update d2l/train.py
    
    Co-Authored-By: Anirudh <anirudhdagar6@gmail.com>
    
    * Update d2l/train.py
    
    Co-Authored-By: Anirudh <anirudhdagar6@gmail.com>
    
    * Update d2l/train.py
    
    Co-Authored-By: Anirudh <anirudhdagar6@gmail.com>
    
    * move net.eval() outside loop
    
    * move net.train() outside trainloop
    
    * removed line 65

--- d2l/train.py
+++ d2l/train.py
@@ -11,17 +11,16 @@ __all__ = ['evaluate_accuracy', 'train_ch3', 'train_ch5']
 
 def evaluate_accuracy(data_iter, net, device=torch.device('cpu')):
     """Evaluate accuracy of a model on the given data set."""
+    acc_sum,n = torch.tensor([0],dtype=torch.float32,device=device),0
     net.eval()  # Switch to evaluation mode for Dropout, BatchNorm etc layers.
-    acc_sum, n = torch.Tensor([0]).to(device), 0
-    for batch in data_iter:
-        features, labels = batch
-        for X, y in zip(features, labels):
-            # If device is the GPU, copy the data to the GPU.
-            y = y.type(torch.float32)
-            X, y = X.to(device), y.to(device)
-            acc_sum += torch.sum((torch.argmax(net(X), dim=1).type(torch.float32) == y).detach()).float()
-            n += 1
-    return acc_sum.item() / n
+    for X, y in data_iter:
+        # If device is the GPU, copy the data to the GPU.
+        X, y = X.to(device), y.to(device)
+        with torch.no_grad():
+            y = y.long()
+            acc_sum += torch.sum((torch.argmax(net(X), dim=1) == y))
+            n += y.shape[0]
+    return acc_sum.item()/n
 
 
 def train_ch3(net, train_iter, test_iter, criterion, num_epochs, batch_size, lr=None):
@@ -47,26 +46,27 @@ def train_ch3(net, train_iter, test_iter, criterion, num_epochs, batch_size, lr=
 
 
 def train_ch5(net, train_iter, test_iter, criterion, num_epochs, batch_size, device, lr=None):
-    net = net.to(device)
+    """Train and evaluate a model with CPU or GPU."""
     print('training on', device)
+    net.to(device)
     optimizer = optim.SGD(net.parameters(), lr=lr)
     for epoch in range(num_epochs):
+        train_l_sum = torch.tensor([0.0],dtype=torch.float32,device=device)
+        train_acc_sum = torch.tensor([0.0],dtype=torch.float32,device=device)
+        n, start = 0, time.time()
         net.train() # Switch to training mode
-        train_l_sum, train_acc_sum, n, start = 0.0, 0.0, 0, time.time()
         for X, y in train_iter:
-            X, y = X.to(device), y.to(device)
             optimizer.zero_grad()
-
+            X,y = X.to(device),y.to(device) 
             y_hat = net(X)
             loss = criterion(y_hat, y)
             loss.backward()
             optimizer.step()
-
-            y = y.type(torch.float32)
-            y.to(device)
-            train_l_sum += loss.item()
-            train_acc_sum += torch.sum((torch.argmax(y_hat, dim=1).type(torch.float32) == y).detach()).float()
-            n += list(y.size())[0]
-        test_acc = evaluate_accuracy(test_iter, net, device)
-        print('epoch %d, loss %.4f, train acc %.3f, test acc %.3f, time %.1f sec'
-              % (epoch + 1, train_l_sum / n, train_acc_sum / n, test_acc, time.time() - start))
+            with torch.no_grad():
+                y = y.long()
+                train_l_sum += loss.float()
+                train_acc_sum += (torch.sum((torch.argmax(y_hat, dim=1) == y))).float()
+                n += y.shape[0]
+        test_acc = evaluate_accuracy(test_iter, net,device)
+         print('epoch %d, loss %.4f, train acc %.3f, test acc %.3f, time %.1f sec'
+              % (epoch + 1, train_l_sum/n, train_acc_sum/n, test_acc, time.time() - start))

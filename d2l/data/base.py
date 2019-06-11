9a457bfd4ac46557ddac11a561cc67bc9d60bfaa
Ajit Pant <43715439+AjitPant@users.noreply.github.com>
2019-06-11 17:59:26 +0530

    Section 10.4 (Text Preprocessing) (#49)

--- d2l/data/base.py
+++ d2l/data/base.py
@@ -1,5 +1,9 @@
 import random
 import collections
+import numpy as np
+import zipfile
+import torch
+import os
 
 class Vocab(object): # This class is saved in d2l.
   def __init__(self, tokens, min_freq=0, use_special_tokens=False):
@@ -35,3 +39,54 @@ class Vocab(object): # This class is saved in d2l.
       return self.idx_to_token[indices]
     else:
       return [self.idx_to_token[index] for index in indices]
+
+
+def data_iter_consecutive(corpus_indices, batch_size, num_steps, ctx=None):
+    # Offset for the iterator over the data for uniform starts
+    offset = int(random.uniform(0,num_steps))
+    # Slice out data - ignore num_steps and just wrap around
+    num_indices = ((len(corpus_indices) - offset) // batch_size) * batch_size
+    indices = torch.Tensor(corpus_indices[offset:(offset + num_indices)], device=ctx)
+    indices = indices.reshape((batch_size,-1))
+    # Need to leave one last token since targets are shifted by 1
+    num_epochs = ((num_indices // batch_size) - 1) // num_steps
+
+    for i in range(0, num_epochs * num_steps, num_steps):
+        X = indices[:,i:(i+num_steps)]
+        Y = indices[:,(i+1):(i+1+num_steps)]
+        yield X, Y
+
+def data_iter_random(corpus_indices, batch_size, num_steps, ctx=None):
+    # Offset for the iterator over the data for uniform starts
+    offset = int(random.uniform(0,num_steps))
+    corpus_indices = corpus_indices[offset:]
+    # Subtract 1 extra since we need to account for the sequence length
+    num_examples = ((len(corpus_indices) - 1) // num_steps) - 1
+    # Discard half empty batches
+    num_batches = num_examples // batch_size
+    example_indices = list(range(0, num_examples * num_steps, num_steps))
+    random.shuffle(example_indices)
+
+    # This returns a sequence of the length num_steps starting from pos
+    def _data(pos):
+        return corpus_indices[pos: pos + num_steps]
+
+    for i in range(0, batch_size * num_batches, batch_size):
+        # Batch_size indicates the random examples read each time
+        batch_indices = example_indices[i:(i+batch_size)]
+        X = [_data(j) for j in batch_indices]
+        Y = [_data(j + 1) for j in batch_indices]
+        yield torch.Tensor(X,  device=ctx), torch.Tensor(Y,  device=ctx)
+
+
+def load_data_time_machine(num_examples=10000):
+    """Load the time machine data set (available in the English book)."""
+    with open('../data/timemachine.txt') as f:
+        raw_text = f.read()
+    lines = raw_text.split('\n')
+    text = ' '.join(' '.join(lines).lower().split())[:num_examples]
+    vocab = Vocab(text)
+    corpus_indices = [vocab[char] for char in text]
+    return corpus_indices, vocab
+
+

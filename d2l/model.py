098478b983b30c341e2db98fe6e8ff869c62b635
Shashank Gupta <guptashashankred@gmail.com>
2019-06-11 10:52:12 +0530

    added Residual Networks 9.6 and added residual block in d2l.model.py

--- d2l/model.py
+++ d2l/model.py
@@ -1,7 +1,33 @@
 """The model module contains neural network building blocks"""
 import torch
+import torch.nn as nn
 
 __all__ = ['corr2d']
+
+class Residual(nn.Module):
+  
+  def __init__(self,input_channels, num_channels, use_1x1conv=False, strides=1, **kwargs):
+    super(Residual, self).__init__(**kwargs)
+    self.conv1 = nn.Conv2d(input_channels, num_channels,kernel_size=3, padding=1, stride=strides)
+    self.conv2 = nn.Conv2d(num_channels, num_channels, kernel_size=3, padding=1)
+    if use_1x1conv:
+      self.conv3 = nn.Conv2d(input_channels, num_channels, kernel_size=1, stride=strides)
+    else:
+      self.conv3 = None
+    self.bn1 = nn.BatchNorm2d(num_channels)
+    self.bn2 = nn.BatchNorm2d(num_channels)
+    self.relu = nn.ReLU(inplace=True)
+  
+  def forward(self, X):
+    
+    Y = self.relu(self.bn1(self.conv1(X)))
+    Y = self.bn2(self.conv2(Y))
+    if self.conv3:
+      X = self.conv3(X)
+    Y += X
+    Y =self.relu(Y)
+    return Y
+
 
 def corr2d(X, K):
     """Compute 2D cross-correlation."""

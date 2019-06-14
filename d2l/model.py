7c5ed21993535f6d39dd2e077daae622390d43ce
Subham103 <subhamsahoo4444@gmail.com>
2019-06-14 19:34:50 +0530

    add RNNModel to d2l and some minor changes

--- d2l/model.py
+++ d2l/model.py
@@ -1,7 +1,9 @@
 """The model module contains neural network building blocks"""
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
-__all__ = ['corr2d', 'linreg']
+__all__ = ['corr2d', 'linreg', 'RNNModel']
 
 def corr2d(X, K):
     """Compute 2D cross-correlation."""
@@ -15,3 +17,25 @@ def corr2d(X, K):
 def linreg(X, w, b):
 	"""Linear regression."""
 	return torch.mm(X,w) + b
+
+class RNNModel(nn.Module):
+    """RNN model."""
+
+    def __init__(self, rnn_layer, num_inputs, vocab_size, **kwargs):
+        super(RNNModel, self).__init__(**kwargs)
+        self.rnn = rnn_layer
+        self.vocab_size = vocab_size
+        self.Linear = nn.Linear(num_inputs, vocab_size)
+
+    def forward(self, inputs, state):
+        """Forward function"""
+        X = F.one_hot(inputs.long().transpose(0,-1), self.vocab_size)
+        X = X.to(torch.float32)
+        state = state.to(torch.float32)
+        Y, state = self.rnn(X, state)
+        output = self.Linear(Y.reshape((-1, Y.shape[-1])))
+        return output, state
+
+    def begin_state(self, num_hiddens, device, batch_size=1):
+        """Return the begin state"""
+        return torch.zeros(size=(1, batch_size, num_hiddens), dtype=torch.int64, device=device)
--- d2l/train.py
+++ d2l/train.py
@@ -4,11 +4,14 @@ import time
 
 from .base import try_gpu
 from .figure import set_figsize, plt
+from .data import data_iter_consecutive, data_iter_random
 
 import torch
+import torch.nn as nn
 import torch.optim as optim
+import torch.nn.functional as F
 
-__all__ = ['evaluate_accuracy', 'squared_loss', 'grad_clipping', 'sgd', 'train_and_predict_rnn', 'train_ch3', 'train_ch5', 'to_onehot' , 'predict_rnn']
+__all__ = ['evaluate_accuracy', 'squared_loss', 'grad_clipping', 'sgd', 'train_and_predict_rnn', 'train_ch3', 'train_ch5', 'to_onehot' , 'predict_rnn', 'train_and_predict_rnn_nn', 'predict_rnn_nn', 'grad_clipping_nn']
 
 
 def evaluate_accuracy(data_iter, net, device=torch.device('cpu')):
@@ -28,15 +31,19 @@ def squared_loss(y_hat, y):
     """Squared loss."""
     return (y_hat - y.view(y_hat.shape)).pow(2) / 2
 
-def grad_clipping(params, theta, ctx):
+def grad_clipping(params, theta, device):
     """Clip the gradient."""
-    norm = torch.Tensor([0], device=ctx)
+    norm = torch.tensor([0], dtype=torch.float32, device=device)
     for param in params:
         norm += (param.grad ** 2).sum()
     norm = norm.sqrt().item()
     if norm > theta:
         for param in params:
             param.grad.data.mul_(theta / norm)
+
+def grad_clipping_nn(model, theta, device):
+    """Clip the gradient for a nn model."""
+    grad_clipping(model.parameters(), theta, device)
 
 def sgd(params, lr, batch_size):
     """Mini-batch stochastic gradient descent."""
@@ -45,14 +52,14 @@ def sgd(params, lr, batch_size):
         param.grad.data.zero_()
 
 def train_and_predict_rnn(rnn, get_params, init_rnn_state, num_hiddens,
-                          corpus_indices, vocab, ctx, is_random_iter,
+                          corpus_indices, vocab, device, is_random_iter,
                           num_epochs, num_steps, lr, clipping_theta,
                           batch_size, prefixes):
     """Train an RNN model and predict the next item in the sequence."""
     if is_random_iter:
-        data_iter_fn = d2l.data_iter_random
+        data_iter_fn = data_iter_random
     else:
-        data_iter_fn = d2l.data_iter_consecutive
+        data_iter_fn = data_iter_consecutive
     params = get_params()
     loss =  nn.CrossEntropyLoss()
     start = time.time()
@@ -60,14 +67,14 @@ def train_and_predict_rnn(rnn, get_params, init_rnn_state, num_hiddens,
         if not is_random_iter:
             # If adjacent sampling is used, the hidden state is initialized
             # at the beginning of the epoch
-            state = init_rnn_state(batch_size, num_hiddens, ctx)
+            state = init_rnn_state(batch_size, num_hiddens, device)
         l_sum, n = 0.0, 0
-        data_iter = data_iter_fn(corpus_indices, batch_size, num_steps, ctx)
+        data_iter = data_iter_fn(corpus_indices, batch_size, num_steps, device)
         for X, Y in data_iter:
             if is_random_iter:
                 # If random sampling is used, the hidden state is initialized
                 # before each mini-batch update
-                state = init_rnn_state(batch_size, num_hiddens, ctx)
+                state = init_rnn_state(batch_size, num_hiddens, device)
             else:
                 # Otherwise, the detach function needs to be used to separate
                 # the hidden state from the computational graph to avoid
@@ -88,8 +95,8 @@ def train_and_predict_rnn(rnn, get_params, init_rnn_state, num_hiddens,
             l = loss(outputs, y.long()).mean()
             l.backward()
             with torch.no_grad():
-                grad_clipping(params, clipping_theta, ctx)  # Clip the gradient
-                d2l.sgd(params, lr, 1)
+                grad_clipping(params, clipping_theta, device)  # Clip the gradient
+                sgd(params, lr, 1)
             # Since the error is the mean, no need to average gradients here
             l_sum += l.item() * y.numel()
             n += y.numel()
@@ -101,7 +108,7 @@ def train_and_predict_rnn(rnn, get_params, init_rnn_state, num_hiddens,
             for prefix in prefixes:
                 print(' -',  predict_rnn(prefix, 50, rnn, params,
                                          init_rnn_state, num_hiddens,
-                                         vocab, ctx))
+                                         vocab, device))
 
 def train_ch3(net, train_iter, test_iter, criterion, num_epochs, batch_size, lr=None):
     """Train and evaluate a model with CPU."""
@@ -156,14 +163,14 @@ def to_onehot(X,size):
     return F.one_hot(X.long().transpose(0,-1), size)
 
 def predict_rnn(prefix, num_chars, rnn, params, init_rnn_state,
-                num_hiddens, vocab, ctx):
+                num_hiddens, vocab, device):
     """Predict next chars with an RNN model"""
-    state = init_rnn_state(1, num_hiddens, ctx)
+    state = init_rnn_state(1, num_hiddens, device)
     output = [vocab[prefix[0]]]
     for t in range(num_chars + len(prefix) - 1):
         # The output of the previous time step is taken as the input of the
         # current time step.
-        X = to_onehot(torch.Tensor([output[-1]],device=ctx), len(vocab))
+        X = to_onehot(torch.tensor([output[-1]], dtype=torch.float32, device=device), len(vocab))
         # Calculate the output and update the hidden state
         (Y, state) = rnn(X, state, params)
         # The input to the next time step is the character in the prefix or
@@ -176,4 +183,57 @@ def predict_rnn(prefix, num_chars, rnn, params, init_rnn_state,
             # use sampling, beam search or beam sampling for better sequences.
             output.append(int(Y[0].argmax(dim=1).item()))
     return ''.join([vocab.idx_to_token[i] for i in output])
+
+def predict_rnn_nn(prefix, num_chars, batch_size, num_hiddens, model, vocab, device):
+    """Predict next chars with a RNN model."""
+    # Use the model's member function to initialize the hidden state
+    state = model.begin_state(num_hiddens=num_hiddens, device=device)
+    output = [vocab[prefix[0]]]
+    for t in range(num_chars + len(prefix) - 1):
+        X = torch.tensor([output[-1]], dtype=torch.float32, device=device).reshape((1, 1))
+        # Forward computation does not require incoming model parameters
+        (Y, state) = model(X, state)
+        if t < len(prefix) - 1:
+            output.append(vocab[prefix[t + 1]])
+        else:
+            output.append(int(Y.argmax(dim=1).item()))
+    return ''.join([vocab.idx_to_token[i] for i in output])
+
+def train_and_predict_rnn_nn(model, num_hiddens, init_gru_state, corpus_indices, vocab,
+                                device, num_epochs, num_steps, lr,
+                                clipping_theta, batch_size, prefixes):
+    """Train a RNN model and predict the next item in the sequence."""
+    loss =  nn.CrossEntropyLoss()
+    optm = torch.optim.SGD(model.parameters(), lr=lr)
+    start = time.time()
+    for epoch in range(1, num_epochs+1):
+        l_sum, n = 0.0, 0
+        data_iter = data_iter_consecutive(
+            corpus_indices, batch_size, num_steps, device)
+        state = model.begin_state(batch_size=batch_size, num_hiddens=num_hiddens, device=device)
+        for X, Y in data_iter:
+            for s in state:
+                s.detach()
+            X = X.to(dtype=torch.long)
+            (output, state) = model(X, state)
+            y = Y.t().reshape((-1,))
+            l = loss(output, y.long()).mean()
+            optm.zero_grad()
+            l.backward(retain_graph=True)
+            with torch.no_grad():
+                # Clip the gradient
+                grad_clipping_nn(model, clipping_theta, device)
+                # Since the error has already taken the mean, the gradient does
+                # not need to be averaged
+                optm.step()
+            l_sum += l.item() * y.numel()
+            n += y.numel()
+
+        if epoch % (num_epochs // 4) == 0:
+            print('epoch %d, perplexity %f, time %.2f sec' % (
+                epoch, math.exp(l_sum / n), time.time() - start))
+            start = time.time()
+        if epoch % (num_epochs // 2) == 0:
+            for prefix in prefixes:
+                print(' -', predict_rnn_nn(prefix, 50, batch_size, num_hiddens, model, vocab, device))
 
